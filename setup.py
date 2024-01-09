fa5c000bdfbc457007286af464784fd00cf6b7e8
soobinseo <soobin323025@gmail.com>
2024-01-09 17:57:30 +0900

    python3.8

--- setup.py
+++ setup.py
@@ -203,6 +203,7 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.8',
         ],
         license='GPLv3',
         python_requires=">=3",
